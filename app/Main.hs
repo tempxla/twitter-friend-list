e5ef70986b0a2eaa696a848aef66cf0ee9670d2d
tempxla <38803749+tempxla@users.noreply.github.com>
2018-06-17 21:45:24 +0900

    fix

--- app/Main.hs
+++ app/Main.hs
@@ -8,14 +8,14 @@ main = do
   createConfigDirectoryIfMissing
   args <- getArgs
   case args of
-    []       -> downloadAndDiff
-    ["diff"] -> diffLatestUserList
-    ["list"] -> listUsers 0
-    ["list", '#' : n] -> listUsers (read n)
-    _        -> do
-      putStrLn   "options: "
-      putStrLn $ "  (no options)" ++ "\t\t" ++ "download and diff list."
-      putStrLn $ "  diff        " ++ "\t\t" ++ "diff latest list. (not download)"
-      putStrLn $ "  list #n     " ++ "\t\t" ++ "print latest list."
-      putStrLn $ "              " ++ "\t\t" ++ "#n is back number option."
-      putStrLn $ "              " ++ "\t\t" ++ "(default #0 which means lastest)"
+    []            -> downloadAndDiff
+    ["diff"]      -> diffLatestUserList
+    ["list"]      -> listUsers 0
+    ["list", n]   -> listUsers (read n)
+    _                 -> do
+      putStrLn   "options:      "
+      putStrLn $ "  (no options)" ++ "\t" ++ "download and diff list.           "
+      putStrLn $ "  diff        " ++ "\t" ++ "diff latest list. (not download)  "
+      putStrLn $ "  list [n]    " ++ "\t" ++ "print latest list. (not download) "
+      putStrLn $ "              " ++ "\t" ++ "n is back number option.          "
+      putStrLn $ "              " ++ "\t" ++ "(default 0, which means lastest)  "
